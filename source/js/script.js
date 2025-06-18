d4ec34a1c2ed89b298fbf307c8ef71f92b7e1cd8
Ely <ely137137@gmail.com>
2025-06-18 23:07:06 +0300

    Create project, set workspace, make some work

---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('hidden');
+});
