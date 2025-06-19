7f83e3768089d48542ffb1efaf098619f71c06bf
Ely <ely137137@gmail.com>
2025-06-19 18:06:04 +0300

    Different image formats & Mobile version

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,17 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     nav.classList.toggle('hidden');
 });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
